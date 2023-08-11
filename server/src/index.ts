fca80d020b1c953e67e7ef941ed19a821c2fa9ce
arthurs14 <arthurcs426@gmail.com>
2023-08-10 17:15:21 -0700

    work on showing the decks from database on page

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,19 +1,39 @@
 import express, { Request, Response } from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 import { config } from "dotenv";
 import Deck from "./models/Deck";
 
 // ENVIRONMENT VARIABLES
 config();
 
+// START SERVER + PORT
 const app = express();
-
-const PORT = 5001;
+const PORT = process.env.PORT || 5002;
 
 // MIDDLEWARE
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 
 // ROUTES
+app.get("/decks", async (req: Request, res: Response) => {
+  try {
+    const decks = await Deck.find();
+
+    res.status(200).json({
+      status: "success",
+      results: decks.length,
+      data: decks,
+    });
+  } catch (err) {
+    res.status(404).json({ status: "fail", message: err });
+  }
+});
+
 app.post("/decks", async (req: Request, res: Response) => {
   try {
     const deck = await Deck.create({
